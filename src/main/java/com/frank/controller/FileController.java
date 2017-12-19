4597c67073bf290a7182c2d85cc5229db9ec385f
13240411778 <13240411778@163.com>
2017-12-19 22:34:39 +0800

    upload

--- src/main/java/com/frank/controller/FileController.java
+++ src/main/java/com/frank/controller/FileController.java
@@ -1,10 +1,23 @@
 package com.frank.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author frank
  * @version 1.0
  * @date 2017/12/17 0017 下午 10:04
  * 文件上传，图片展示，流相关操作记录
  */
+@Slf4j
+@Controller
+@RequestMapping(value = "/file")
 public class FileController {
+
+    @RequestMapping("/banner")
+    public String banner(){
+        //  map.put("hello","hello");
+        return"/view/banner";
+    }
 }
