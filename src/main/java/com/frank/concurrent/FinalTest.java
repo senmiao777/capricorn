3805068c964a078218452396786084094d550f46
13240411778 <13240411778@163.com>
2018-01-07 11:28:22 +0800

    threadlocal 单元测试

--- src/main/java/com/frank/concurrent/FinalTest.java
+++ src/main/java/com/frank/concurrent/FinalTest.java
@@ -3,6 +3,8 @@ package com.frank.concurrent;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.RandomUtils;
 
+import java.util.function.Supplier;
+
 /**
  * @author frank
  * @version 1.0
@@ -13,7 +15,19 @@ public class FinalTest {
     public final String STR = String.valueOf(RandomUtils.nextInt());
     public static final String STATIC_STR = String.valueOf(RandomUtils.nextInt());
 
+    final ThreadLocal<Integer> threadRandomNumber = ThreadLocal.withInitial(new Supplier<Integer>() {
+        @Override
+        public Integer get() {
+            // TODO Auto-generated method stub
+            return new Integer(8);
+        }
+    });
+
     public void append(final StringBuilder builder,String str){
         builder.append(str);
+    }
+
+    public void doSomething(){
+        threadRandomNumber.get();
     }
 }
