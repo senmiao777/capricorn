e3889d7eb11ed5dd9697c6331e54d3a630b333e9
guojingfeng <jingfeng.guo@quantgroup.cn>
2018-01-31 14:08:16 +0800

    LinkedBlockingQueue

--- src/main/java/com/frank/concurrent/Consumer.java
+++ src/main/java/com/frank/concurrent/Consumer.java
@@ -19,6 +19,19 @@ public class Consumer implements Runnable {
      * LinkedBlockingQueue对于生产者端和消费者端分别采用了独立的锁来控制数据同步，
      * 这也意味着在高并发的情况下生产者和消费者可以并行地操作队列中的数据，
      * 以此来提高整个队列的并发性能。
+     * <p>
+     * ArrayBlockingQueue和LinkedBlockingQueue的区别：
+     * 1. 队列中锁的实现不同
+     * ArrayBlockingQueue实现的队列中的锁是没有分离的，即生产和消费用的是同一个锁；
+     * LinkedBlockingQueue实现的队列中的锁是分离的，即生产用的是putLock，消费是takeLock
+     * <p>
+     * 2. 在生产或消费时操作不同
+     * ArrayBlockingQueue实现的队列中在生产和消费的时候，是直接将枚举对象插入或移除的；
+     * LinkedBlockingQueue实现的队列中在生产和消费的时候，需要把枚举对象转换为Node<E>进行插入或移除，会影响性能
+     * <p>
+     * 3. 队列大小初始化方式不同
+     * ArrayBlockingQueue实现的队列中必须指定队列的大小；
+     * LinkedBlockingQueue实现的队列中可以不指定队列的大小，但是默认是Integer.MAX_VALUE
      */
     private BlockingQueue<String> queue;
 
