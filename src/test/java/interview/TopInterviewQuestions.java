8de56c79880489149f96a81f452847c12d12a18d
13240411778 <13240411778@163.com>
2018-03-09 22:22:33 +0800

    Add Two Numbers

--- src/test/java/interview/TopInterviewQuestions.java
+++ src/test/java/interview/TopInterviewQuestions.java
@@ -2,11 +2,7 @@ package interview;
 
 import com.frank.enums.Common;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.util.Lists;
 import org.junit.Test;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author frank
@@ -18,11 +14,6 @@ public class TopInterviewQuestions {
     @Test
     public void addTwoNumbers() {
         log.info(Common.LOG_BEGIN.getValue());
-        List<Integer> firstList = new ArrayList<>();
-        List<Integer> secondList = Lists.newArrayList(5,6,4);
-
-        StringBuilder firstNumberString = new StringBuilder();
-
         ListNode l1 = new ListNode(2);
 
         ListNode l2 = new ListNode(4);
@@ -36,54 +27,20 @@ public class TopInterviewQuestions {
         l4.setNext(l5);
         l5.setNext(l6);
 
-        ListNode currentNode;
-        ListNode nextNode;
-
-        int sum = getNumber(l1)+getNumber(l4);
-//        currentNode = l1;
-//
-//        do{
-//            nextNode = currentNode.getNext();
-//
-//            firstNumberString.append(currentNode.getVal());
-//            currentNode = nextNode;
-//
-//        }while(nextNode!=null);
-//
-//        Integer firstNumber = Integer.valueOf(firstNumberString.toString());
-//
-//
-//
-//
-//
-//
-//        StringBuilder secondNumberString = new StringBuilder();
-//        currentNode = l4;
-//        do{
-//            nextNode = currentNode.getNext();
-//
-//            secondNumberString.append(currentNode.getVal());
-//            currentNode = nextNode;
-//
-//        }while(nextNode!=null);
-//
-//        Integer secondNumber = Integer.valueOf(secondNumberString.toString());
-//
-//        int sum = firstNumber + secondNumber;
-        log.info("sum={}",sum);
+        int sum = getNumber(l1) + getNumber(l4);
+        log.info("sum={}", sum);
 
         log.info(Common.LOG_END.getValue());
     }
 
-    private int getNumber(ListNode currentNode){
+    private int getNumber(ListNode currentNode) {
         StringBuilder numberString = new StringBuilder();
-       // ListNode currentNode = node;
         ListNode nextNode;
-        do{
+        do {
             nextNode = currentNode.getNext();
             numberString.append(currentNode.getVal());
             currentNode = nextNode;
-        }while(nextNode!=null);
+        } while (nextNode != null);
         return Integer.valueOf(numberString.toString());
     }
 
