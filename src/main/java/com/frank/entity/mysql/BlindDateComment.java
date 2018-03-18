4b43dc2299b7a19472030b16f2e0131fb4a6eee9
13240411778 <13240411778@163.com>
2018-03-18 16:59:32 +0800

    sql2Entity

---
+++ src/main/java/com/frank/entity/mysql/BlindDateComment.java
@@ -0,0 +1,10 @@
+package com.frank.entity.mysql;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2018/3/18 0018 下午 4:31
+ */
+public class BlindDateComment {
+
+}
--- src/test/java/StockTest.java
+++ src/test/java/StockTest.java
@@ -14,6 +14,7 @@ import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.assertj.core.util.Lists;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
@@ -84,9 +85,60 @@ public class StockTest {
     }
 
     @Test
-    public void testAtomicLong() {
+    public void sql2Entity() {
         long l = num.incrementAndGet();
 
+        List<String> attrList = Lists.newArrayList("id bigint(20) NOT NULL AUTO_INCREMENT",
+                "area varchar(40)",
+                "occupation varchar(40)",
+                "requirement varchar(256) ",
+                "content varchar(1024) ",
+                "user_id varchar(30) ",
+                "user_name varchar(64) ",
+                "url_token varchar(100) ",
+                "head_image varchar(256) ",
+                "gender tinyint(1) default 1",
+                "register_gender tinyint(1) default 1",
+                "register_at bigint(20) ",
+                "headline varchar(256) ");
+
+        String perfix = "/**\n" +
+                "     * //TODO\n" +
+                "     */";
+
+        StringBuilder sql = new StringBuilder();
+        for (String a : attrList) {
+            List<String> strings = Splitter.on(" ").splitToList(a);
+            sql.append(perfix).append(getAttributeType(strings.get(1))).append(strings.get(0)).append(";\n\n");;
+        }
+        log.info("sql = {}",sql.toString());
+
+
+    }
+
+    private String getAttributeType(String type) {
+        if (type.contains("varchar")) {
+            return "private String ";
+        } else if (type.contains("bigint")) {
+            return "private Long ";
+        } else if (type.contains("int")) {
+            return "private Integer ";
+        } else {
+            return "private String ";
+        }
+    }
+
+    /**
+     * 拼接实体类
+     *
+     * @param s
+     * @param list
+     * @return
+     */
+    private StringBuilder get2CurrentString(StringBuilder s, List<String> list) {
+        s.append("/**").append(list.get(2)).append("*/").append("@Column(name = \"").append(list.get(0))
+                .append("\")").append("private ").append(getType(list.get(1))).append(" ").append(list.get(0)).append(";\n\n");
+        return s;
     }
 
 
