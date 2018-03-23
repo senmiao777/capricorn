f04a491953c1cb0a540791e0fd361ec9e27728a9
13240411778 <13240411778@163.com>
2018-03-23 17:02:48 +0800

    数据初步处理

---
+++ src/main/java/com/frank/concurrent/Alternate.java
@@ -0,0 +1,13 @@
+package com.frank.concurrent;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2018/3/23 0023 下午 4:49
+ */
+public class Alternate implements Runnable {
+    @Override
+    public void run() {
+
+    }
+}
