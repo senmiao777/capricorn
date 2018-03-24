e7c932c37102a236c5fc5ec0e2b589405ffdbdf3
13240411778 <13240411778@163.com>
2018-03-24 13:11:41 +0800

    condition

--- src/main/java/com/frank/concurrent/Alternate.java
+++ src/main/java/com/frank/concurrent/Alternate.java
@@ -1,13 +1,91 @@
 package com.frank.concurrent;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author frank
  * @version 1.0
  * @date 2018/3/23 0023 下午 4:49
  */
+@Slf4j
 public class Alternate implements Runnable {
+
+    private Lock lock = new ReentrantLock();
+    private int t = 0;
+    private int j = 1;
+    Condition first = lock.newCondition();
+    Condition second = lock.newCondition();
+    Condition third = lock.newCondition();
+
+    public void one() {
+        lock.lock();
+        try {
+            if (j != 0) {
+                first.await();
+            }
+            log.info("first--------------");
+            j = (j + 1) % 3;
+            second.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void two() {
+        lock.lock();
+        try {
+            if (j != 1) {
+                second.await();
+            }
+            log.info("second--------------");
+            j = (j + 1) % 3;
+            third.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+    public void three() {
+        lock.lock();
+        try {
+            if (j != 2) {
+                third.await();
+            }
+            log.info("third--------------");
+            j = (j + 1) % 3;
+            first.signal();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
     @Override
     public void run() {
+        for (int i = 0; i < 10; i++) {
+            if (t == 0) {
+                one();
+                t = (t + 1) % 3;
+            } else if (t == 1) {
 
+                two();
+                t = (t + 1) % 3;
+
+            } else {
+                three();
+                t = (t + 1) % 3;
+            }
+        }
     }
 }
