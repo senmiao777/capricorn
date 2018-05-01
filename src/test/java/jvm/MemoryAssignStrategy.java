3eed9445ae250922832137c89e4f2da3c1d918dc
13240411778 <13240411778@163.com>
2018-05-01 10:57:54 +0800

    新对象有现在Eden区分配

---
+++ src/test/java/jvm/MemoryAssignStrategy.java
@@ -0,0 +1,22 @@
+package jvm;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2018年5月1日10:54:34
+ */
+@Slf4j
+public class MemoryAssignStrategy {
+
+    /**
+     * 对象优先在Eden分配
+     */
+    @Test
+    public void eden() {
+
+    }
+
+}
