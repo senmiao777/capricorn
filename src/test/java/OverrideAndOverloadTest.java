734eb206ae77ee5f97462678b143031b6fa0f7ec
13240411778 <13240411778@163.com>
2018-06-04 21:31:31 +0800

    编译器虽然能确定出方法的重载版本，但在很多情况下这个重载的版本并不是唯一的，往往只能确定一个更加合适的版本。

--- src/test/java/OverrideAndOverloadTest.java
+++ src/test/java/OverrideAndOverloadTest.java
@@ -30,12 +30,25 @@ public class OverrideAndOverloadTest {
     }
 
 
+    /**
+     * 重载测试
+     *
+     */
     @Test
     public void testHello() {
+        /**
+         * Human 称为 外观类型 或者 静态类型
+         * Man 称为 实际类型
+         */
         Human man = new Man();
         Human woman = new Woman();
         OverrideAndOverloadTest t = new OverrideAndOverloadTest();
 
+        /**
+         * 方法的接受者在确定调用方法的对象的前提下，使用哪个重载（overload）版本，取决于传入参数的数据量和类型
+         * 虚拟机，准确的说是编译器，在重载的时候是通过参数的静态类型而不是实际类型作为判定依据的
+         * 所以，下边的代码，输出的是 Human sayHello
+         */
         // Human sayHello
         t.sayHello(man);
         // Human sayHello
@@ -43,5 +56,9 @@ public class OverrideAndOverloadTest {
 
         t.sayHello((Man) man);
         t.sayHello((Woman) woman);
+
+        /**
+         * 编译器虽然能确定出方法的重载版本，但在很多情况下这个重载的版本并不是唯一的，往往只能确定一个更加合适的版本。
+         */
     }
 }
