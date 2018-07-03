20d5e338ac0738e7cc4571bc6eb0754e84606694
guojingfeng <jingfeng.guo@quantgroup.cn>
2018-07-03 18:18:55 +0800

    观察者模式

---
+++ src/main/java/com/frank/designpattern/Observable.java
@@ -0,0 +1,31 @@
+package com.frank.designpattern;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2018/7/3
+ * <p>
+ * 被观察者
+ */
+public interface Observable {
+    /**
+     * 添加观察者
+     *
+     * @param observer
+     */
+    void addObserver(Observer observer);
+
+    /**
+     * 删除观察者
+     *
+     * @param observer
+     */
+    void deleteObserver(Observer observer);
+
+    /**
+     * 通知观察者
+     *
+     * @param observer
+     */
+    void notifyObservers(Observer observer);
+}
