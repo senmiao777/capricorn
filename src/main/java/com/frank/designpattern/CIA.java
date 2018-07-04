c16013746204796d6c50e4671f50e655f8accfc4
guojingfeng <jingfeng.guo@quantgroup.cn>
2018-07-04 11:14:24 +0800

    观察者模式 说明

--- src/main/java/com/frank/designpattern/CIA.java
+++ src/main/java/com/frank/designpattern/CIA.java
@@ -1,6 +1,7 @@
 package com.frank.designpattern;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 
 /**
  * @author frank
@@ -10,6 +11,8 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class CIA implements Observer {
+
+    @Async("testTaskPoolExecutor")
     @Override
     public void action(Observable.Notice notice) {
         log.info("CIA监听到{} {}活动，内容{}", notice.getObservable(), notice.getOperationType(), notice.getObj());
--- src/main/java/com/frank/designpattern/Police.java
+++ src/main/java/com/frank/designpattern/Police.java
@@ -1,6 +1,7 @@
 package com.frank.designpattern;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 
 /**
  * @author frank
@@ -10,8 +11,17 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class Police implements Observer {
+
+    @Async("testTaskPoolExecutor")
     @Override
     public void action(Observable.Notice notice) {
+        try {
+            // 模拟延迟
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         log.info("Police监听到{} {}活动，内容{}", notice.getObservable(), notice.getOperationType(), notice.getObj());
+
     }
 }
