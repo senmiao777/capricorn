3a744f61f05152225cc5eed466d141e11b09c660
13240411778 <13240411778@163.com>
2019-03-03 19:16:51 +0800

    策略模式

--- src/main/java/com/frank/designpattern/strategy/Pistol.java
+++ src/main/java/com/frank/designpattern/strategy/Pistol.java
@@ -1,13 +1,16 @@
 package com.frank.designpattern.strategy;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author frank
  * @version 1.0
  * @date 2019/3/3 0003 下午 6:47
  */
+@Slf4j
 public class Pistol implements Fightable {
     @Override
     public void fight() {
-
+        log.info("用手枪战斗");
     }
 }
