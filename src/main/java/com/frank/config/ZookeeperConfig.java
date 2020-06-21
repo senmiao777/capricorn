f9940ed641d67aeb612dc24ed7894fcc43a4857c
13240411778 <13240411778@163.com>
2020-06-21 18:35:34 +0800

    elastic job

---
+++ src/main/java/com/frank/config/ZookeeperConfig.java
@@ -0,0 +1,23 @@
+package com.frank.config;
+
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperConfiguration;
+import com.dangdang.ddframe.job.reg.zookeeper.ZookeeperRegistryCenter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2020/6/19 0019 下午 10:56
+ */
+@Configuration
+@ConditionalOnExpression("'${regCenter.serverList}'.length() > 0")
+public class ZookeeperConfig {
+    @Bean(initMethod = "init")
+    public ZookeeperRegistryCenter regCenter(@Value("${regCenter.serverList}") final String serverList,
+                                             @Value("${regCenter.namespace}") final String namespace) {
+        return new ZookeeperRegistryCenter(new ZookeeperConfiguration(serverList, namespace));
+    }
+}
