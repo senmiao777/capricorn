eb9afeffbf03b47736a8bde0fba289c699314cdd
senmiao777 <13240411778@163.com>
2021-05-22 18:18:50 +0800

    PriorityQueue

--- src/test/java/concurrent/CommonTest.java
+++ src/test/java/concurrent/CommonTest.java
@@ -3,6 +3,8 @@ package concurrent;
 import org.junit.Test;
 
 import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
@@ -28,7 +30,38 @@ public class CommonTest {
         }, 2, 1, TimeUnit.SECONDS);
         Thread.sleep(5000L);
 
+
         System.out.println("当前时间2:" + LocalDateTime.now().getSecond());
+
+
+        // 构建一个二叉堆, 内部存放int数据
+        Integer[] data = {3, 5, 9, 1, 10, 8, 2, 12, 7, 4};
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return Integer.valueOf(o2.toString()) - Integer.valueOf(o1.toString());
+            }
+        });
+
+
+        /**
+         * 正序最小堆
+         * 倒序最大堆
+         *
+         */
+        PriorityQueue<Integer> queue2 = new PriorityQueue<>((o1, o2) -> o2 - o1);
+        for (int i = 0; i < data.length; i++) {
+            queue2.offer(data[i]);
+        }
+
+
+        System.out.println("============== 最小堆示例 ==============");
+        for (int i = 0; i < data.length; i++) {
+            System.out.println(queue2.poll());
+        }
+
+
     }
 
 }
