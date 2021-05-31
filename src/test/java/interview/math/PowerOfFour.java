65697293dbd7bd87c453832c2c1454128d6a9446
guojingfeng <guojingfeng@58.com>
2021-05-31 20:38:16 +0800

    与运算

--- src/test/java/interview/math/PowerOfFour.java
+++ src/test/java/interview/math/PowerOfFour.java
@@ -28,10 +28,27 @@ public class PowerOfFour {
         if (number <= 0) {
             return false;
         }
-        int temp =4;
-        while (number % temp == 0 && number >temp) {
-            temp *=4;
+        int temp = 4;
+        while (number % temp == 0 && number > temp) {
+            temp *= 4;
         }
         return (number == 1) || (number == temp);
+    }
+
+    /**
+     * 可以构造一个整数 mask，它的所有偶数二进制位都是 0，所有奇数二进制位都是 1。
+     * 这样一来，我们将 n 和 mask 进行按位与运算，如果结果为 0，说明 n 二进制表示中的 1 出现在偶数的位置，否则说明其出现在奇数的位置。
+     * <p>
+     * 根据上面的思路，\textit{mask}mask 的二进制表示为：
+     * mask=(10101010101010101010101010101010)  java中是0b开头表示二进制0b10101010101010101010101010101010
+     * <p>
+     * 我们也可以将其表示成 1616 进制的形式，使其更加美观：
+     * mask=(AAAAAAAA) 16 Java中是0x开头表示16进制0xaaaaaaaa
+     * 链接：https://leetcode-cn.com/problems/power-of-four/solution/4de-mi-by-leetcode-solution-b3ya/
+     */
+    public boolean powerOf4(int n) {
+        //return (n > 0) && ((n & n - 1) == 0) && ((n & 0xaaaaaaaa) == 0);
+        return (n > 0) && ((n & n - 1) == 0) && ((n & 0b10101010101010101010101010101010) == 0);
+
     }
 }
