fb2de745f8314a4591a841db3ca188d40adc2461
senmiao777 <13240411778@163.com>
2021-06-14 13:05:14 +0800

    IntStream.range(0, 30)

--- src/main/java/com/frank/concurrent/FinalTest.java
+++ src/main/java/com/frank/concurrent/FinalTest.java
@@ -12,7 +12,7 @@ import org.apache.commons.lang3.RandomUtils;
 public class FinalTest {
     public final String STR = String.valueOf(RandomUtils.nextInt());
     public static final String STATIC_STR = String.valueOf(RandomUtils.nextInt());
-
+    ThreadLocal<Integer> test =new ThreadLocal<>();
     final static ThreadLocal<Integer> threadRandomNumber = ThreadLocal.withInitial(() -> {
         return RandomUtils.nextInt();
     });
@@ -38,5 +38,34 @@ public class FinalTest {
     public void setNumber(Integer number) {
         log.info("setNumber call number={},ThreadId={}", number, Thread.currentThread().getId());
         threadRandomNumber.set(number);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread first = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                str.set("111");
+                str2.set("222");
+                System.out.println(Thread.currentThread().getName() + "value="+str.get() +",value2="+str2.get());
+                str2.remove();
+                str.remove();
+            }
+        },"Thread-1");
+
+        Thread second = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                str.set("333");
+                str2.set("444");
+                System.out.println(Thread.currentThread().getName() + "value="+str.get() +",value2="+str2.get());
+            }
+        },"Thread-2");
+
+
+        first.start();
+        second.start();
+        Thread.sleep(1000L);
+
+
     }
 }
---
+++ src/test/java/concurrent/CountDownLatchTest.java
@@ -0,0 +1,77 @@
+package concurrent;
+
+import lombok.SneakyThrows;
+import org.apache.commons.lang3.RandomUtils;
+import org.junit.Test;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.CyclicBarrier;
+import java.util.stream.IntStream;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2021/6/14 0014 下午 12:05
+ * 模拟高并发
+ */
+public class CountDownLatchTest {
+    final int NUMBER = 30;
+
+    /**
+     * 确保在countdownlatch没有到0之前，实际业务不会执行
+     */
+    CountDownLatch count = new CountDownLatch(NUMBER);
+    /**
+     * 就绪判断依据
+     * 新建线程数N + main线程数1
+     * <p>
+     * 人齐了才开始 XX
+     */
+    CyclicBarrier ready = new CyclicBarrier(NUMBER + 1);
+    int stock = 200;
+
+    class StockTask implements Runnable {
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            try {
+                count.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            if (stock >= 3) {
+                int temp = stock;
+                Thread.sleep(RandomUtils.nextInt(1, 30));
+                stock -= 3;
+                System.out.println("当前库存为" + temp + "扣减之后为" + stock);
+            } else {
+                System.out.println("库存不足，为" + stock);
+            }
+            // 表示已就绪，完成
+            ready.await();
+        }
+    }
+
+    @Test
+    public void subStockTest() throws InterruptedException, BrokenBarrierException {
+        final long begin = System.currentTimeMillis();
+        /**
+         * 启动30个线程
+         */
+        for (int i = 0; i < NUMBER; i++) {
+            new Thread(new StockTask()).start();
+            count.countDown();
+        }
+        ready.await();
+        System.out.println("最终库存为" + stock + "耗时=" + (System.currentTimeMillis() - begin));
+
+        // lambda 写法
+        IntStream.range(0, 30).forEach(s -> {
+            System.out.println("第" + (s + 1) + "次执行");
+            new Thread(new StockTask()).start();
+            count.countDown();
+        });
+    }
+}
