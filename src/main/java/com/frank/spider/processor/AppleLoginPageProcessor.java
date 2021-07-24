f345f281ef0ca24268c9e996750eb15097f7d7b8
senmiao777 <13240411778@163.com>
2021-07-24 09:24:00 +0800

    web magic pageprocessor import

---
+++ src/main/java/com/frank/spider/processor/AppleLoginPageProcessor.java
@@ -0,0 +1,22 @@
+package com.frank.spider.processor;
+
+import us.codecraft.webmagic.Page;
+import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.processor.PageProcessor;
+
+/**
+ * @author frank
+ * @version 1.0
+ * @date 2021/7/18 0018 上午 11:18
+ */
+public class AppleLoginPageProcessor implements PageProcessor {
+    @Override
+    public void process(Page page) {
+
+    }
+
+    @Override
+    public Site getSite() {
+        return null;
+    }
+}
