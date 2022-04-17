42504e5bd45b96c39409f3c89f43ced3549def28
senmiao777 <13240411778@163.com>
2022-04-17 10:48:32 +0800

    send message

---
+++ src/main/java/com/frank/message/Message.java
@@ -0,0 +1,44 @@
+package com.frank.message;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author Administrator
+ */
+public class Message {
+
+    /**
+     * 用户名
+     */
+    private static String Uid = "woxiangjingjing";
+
+    /**
+     * 接口安全秘钥
+     */
+    private static String Key = "d41d8cd98f00b204e980";
+
+    /**
+     * 短信号码，多个号码如13800000000,13800000001,13800000002
+     */
+    private static String smsMob = "13240411778";
+
+    /**
+     * 短信内容
+     */
+    private static String smsText = "【货物更新】订单号86-"+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+
+
+    public static void main(String[] args) {
+
+        HttpClientUtil client = HttpClientUtil.getInstance();
+
+        //UTF发送
+        int result = client.sendMsgUtf8(Uid, Key, smsText, smsMob);
+        if(result>0){
+            System.out.println("UTF8成功发送条数=="+result);
+        }else{
+            System.out.println(client.getErrorMsg(result));
+        }
+    }
+}
---
+++ src/test/java/algorithm/TreeNodeTest.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+
+@Slf4j
+public class TreeNodeTest {
+
+
+
+
+    @Test
+    public void testShowAllNodes(){
+        // 构造二叉查找树
+
+    }
+
+
+}
