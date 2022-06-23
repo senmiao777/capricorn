460f6cacbf6f9efbe642e6aff7af6209c1a6ae33
guojingfeng <guojingfeng@58.com>
2022-06-23 19:59:44 +0800

    reverse

--- src/test/java/interview/linkedlist/LinkedListTest.java
+++ src/test/java/interview/linkedlist/LinkedListTest.java
@@ -2,6 +2,7 @@ package interview.linkedlist;
 
 import com.frank.model.leetcode.ListNode;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.context.annotation.Bean;
 
 /**
@@ -11,19 +12,54 @@ import org.springframework.context.annotation.Bean;
  **/
 public class LinkedListTest {
 
+    public ListNode head;
 
     @Before
-    public void init(){
+    public void init() {
         ListNode one = new ListNode(1);
         ListNode two = new ListNode(2);
         ListNode three = new ListNode(3);
         ListNode four = new ListNode(4);
-        ListNode five = new ListNode(3);
-        ListNode six = new ListNode(4);
+        ListNode five = new ListNode(5);
+        ListNode six = new ListNode(6);
         one.next = two;
         two.next = three;
         three.next = four;
         four.next = five;
         five.next = six;
+        six.next = null;
+        head = one;
+    }
+
+    @Test
+    public void testReverse() {
+        ListNode reverse = reverse(head);
+        while (reverse != null) {
+            System.out.println(reverse.val);
+            reverse = reverse.next;
+        }
+    }
+
+    /**
+     * 单向链表翻转
+     * 最后指向null 不会处理
+     * @param head
+     */
+    private ListNode reverse(ListNode head) {
+
+
+        if (head == null) {
+            return null;
+        }
+        ListNode temp = head;
+        ListNode tail = null;
+        while (head != null) {
+            temp = head;
+            head = head.next;
+            temp.next = tail;
+            tail = temp;
+        }
+
+        return temp;
     }
 }
