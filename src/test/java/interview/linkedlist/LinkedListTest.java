39060e3be02e7ce7be6b6d532c90696c3712124c
guojingfeng <guojingfeng@58.com>
2022-06-24 19:29:05 +0800

    LockSupportTest

--- src/test/java/interview/linkedlist/LinkedListTest.java
+++ src/test/java/interview/linkedlist/LinkedListTest.java
@@ -55,12 +55,35 @@ public class LinkedListTest {
 
     @Test
     public void testHasCycle() {
+        tail.next = head;
 
+        boolean res = hasCycle(head);
+
+        System.out.println("hasCycle=" +res);
     }
+
+    private boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+            if (fast != null) {
+                fast = fast.next;
+            }
+            if (fast == slow) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     /**
      * 单向链表翻转
-
      *
      * @param head
      */
