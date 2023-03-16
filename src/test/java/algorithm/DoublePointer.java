615eedfb3d152e6cdac9c28508fcf2becbbbc629
guojingfeng <guojingfeng@58.com>
2023-03-16 15:46:54 +0800

    getLengthOfLongestSubString

--- src/test/java/algorithm/DoublePointer.java
+++ src/test/java/algorithm/DoublePointer.java
@@ -5,12 +5,125 @@ import com.frank.model.leetcode.ListNode;
 import org.junit.Test;
 
 import javax.print.DocFlavor;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class DoublePointer {
+
+
+    /**
+     * 733. 图像渲染
+     * 有一幅以 m x n 的二维整数数组表示的图画 image ，其中 image[i][j] 表示该图画的像素值大小。
+     * 你也被给予三个整数 sr ,  sc 和 newColor 。你应该从像素 image[sr][sc] 开始对图像进行 上色填充 。
+     * 为了完成 上色工作 ，从初始像素开始，记录初始坐标的 上下左右四个方向上 像素值与初始坐标相同的相连像素点，接着再记录这四个方向上符合条件的像素点与他们对应 四个方向上 像素值与初始坐标相同的相连像素点，……，重复该过程。将所有有记录的像素点的颜色值改为 newColor 。
+     * 最后返回 经过上色渲染后的图像 。
+     */
+    @Test
+    public void tsw() {
+        int[][] image = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+        int row = image.length;
+        int column = image[0].length;
+        System.out.println("row=" + row + "  column=" + column);
+        printTwoDimensionalArray(image);
+        floodFill(image, 1, 1, 2);
+        printTwoDimensionalArray(image);
+    }
+
+    private void printTwoDimensionalArray(int[][] image) {
+        int row = image.length;
+        int column = image[0].length;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < column; j++) {
+                System.out.print(image[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 二维数组几个元素就是几行，每个元素（数组）由几个元素组成就是几列
+     *
+     * @param image
+     * @param sr
+     * @param sc
+     * @param color
+     * @return
+     */
+    public void floodFill(int[][] image, int sr, int sc, int color) {
+        dfs(image, sr, sc, color, image[sr][sc]);
+    }
+
+    /**
+     * 深度优先遍历
+     *
+     * @param image
+     * @param sr
+     * @param sc
+     * @param color
+     * @param originColor
+     */
+    private void dfs(int[][] image, int sr, int sc, int color, int originColor) {
+        if (sr < 0 || sc < 0
+                || sr >= image.length || sc >= image[0].length
+                || color == originColor
+                || image[sr][sc] != originColor
+
+        ) {
+            return;
+        }
+        image[sr][sc] = color;
+        dfs(image, sr + 1, sc, color, originColor);
+        dfs(image, sr - 1, sc, color, originColor);
+        dfs(image, sr, sc + 1, color, originColor);
+        dfs(image, sr, sc - 1, color, originColor);
+    }
+
+    /**
+     * 广度优先遍历
+     * <p>
+     * 思路：不断把符合条件的坐标添加到队列中
+     * 要点：
+     * 1：往小了找，是判断边界大于等于 零；往打了找，是判断边界小于等于 length - 1
+     * 2：边界是大于等于/小于等于，都是带着等于的
+     *
+     * @param image
+     * @param sr
+     * @param sc
+     * @param color
+     * @return
+     */
+    private int[][] bfs(int[][] image, int sr, int sc, int color) {
+        if (image == null || image.length == 0 || image[0].length == 0 || image[sr][sc] == color) {
+            return image;
+        }
+        int rNum = image.length - 1;
+        int cNum = image[0].length - 1;
+        int rawColor = image[sr][sc];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{sr, sc});
+
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int r = poll[0];
+            int c = poll[1];
+            image[r][c] = color;
+            if (r - 1 >= 0 && image[r - 1][c] == rawColor) {
+                queue.offer(new int[]{r - 1, c});
+            }
+            if (r + 1 <= rNum && image[r + 1][c] == rawColor) {
+                queue.offer(new int[]{r + 1, c});
+            }
+            if (c - 1 >= 0 && image[r][c - 1] == rawColor) {
+                queue.offer(new int[]{r, c - 1});
+            }
+
+            if (c + 1 <= cNum && image[r][c + 1] == rawColor) {
+                queue.offer(new int[]{r, c + 1});
+            }
+        }
+
+        return image;
+    }
+
 
     /**
      * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
@@ -29,6 +142,7 @@ public class DoublePointer {
      * 两个关键点：
      * 1：start = Math.max(position + 1, start);
      * 2：res = Math.max(res, i - start + 1);
+     *
      * @param s
      * @return
      */
